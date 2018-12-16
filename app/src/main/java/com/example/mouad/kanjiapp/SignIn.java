77bf171c28a6de5b59a685751f9bb45f222d50b5
mouad <mouadsehbaoui]@gmail.com>
2018-12-16 11:00:12 +0100

    missing files in the git repertory

--- app/src/main/java/com/example/mouad/kanjiapp/SignIn.java
+++ app/src/main/java/com/example/mouad/kanjiapp/SignIn.java
@@ -1,27 +1,19 @@
 package com.example.mouad.kanjiapp;
 
-import android.content.ContentValues;
 import android.content.Intent;
 import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
-import android.os.Environment;
 import android.provider.MediaStore;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.Serializable;
 
